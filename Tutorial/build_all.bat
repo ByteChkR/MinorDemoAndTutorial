ddabbf5fe077ff55e0dadaaa459d20d39777effc
Tim Akermann <ti.akermann@gmail.com>
2019-12-14 13:55:29 +0100

    Updated Engine Version and Added OpenFL_Runner into the build script.

--- Tutorial/build_all.bat
+++ Tutorial/build_all.bat
@@ -3,6 +3,7 @@ call build.bat AI
 call build.bat Audio
 call build.bat OpenCL_Begin
 call build.bat OpenFL_Begin
+call build.bat OpenFL_Runner
 call build.bat GettingStarted
 call build.bat Physics
 call build.bat Raycasting
